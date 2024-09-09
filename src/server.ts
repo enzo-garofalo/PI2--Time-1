b87c8e4119f3dd3dd5385b941c71bfce0025e31b
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-09-08 21:17:20 -0300

    Openning Server!

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,10 @@
-const m:string = "Eita mundo bom!";
-console.log(m);
+import http from 'http';
+
+const server = http.createServer((req, res) =>{
+  res.writeHead(200, {"Content-Type" : "Text-Plain"});
+  res.end("Servidor da PucBet funcionando");
+});
+
+server.listen(3000, () =>{
+  console.log('Eita mundo bom! Na porta 3000.');
+});
