fede4692c2b322ca16ce9d2c28357ef5ab0e4de9
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-09-23 21:34:08 -0300

    Corrigindo bugs

--- src/server.ts
+++ src/server.ts
@@ -3,57 +3,57 @@ import http from 'http';
 const server = http.createServer((req, res) =>{
   if(req.url === '/signUp')
   {
-    res.writeHead(200, {"Content-Type" : "Text-Plain"});
+    res.writeHead(200, {"Content-Type" : "text/plain"});
     res.end("Pagina de Sign UP");
   }
   else if(req.url === '/login')
   {
-    res.writeHead(200, {"Content-Type" : "Text-Plain"});
+    res.writeHead(200, {"Content-Type" : "text/plain"});
     res.end("Pagina de Login");
   }
   else if(req.url === '/addNewEvent')
   {
-    res.writeHead(200, {"Content-Type" : "Text-Plain"});
+    res.writeHead(200, {"Content-Type" : "text/plain"});
     res.end("Pagina de Novo Evento");
   }
   else if(req.url === '/getEvent')
   {
-    res.writeHead(200, {"Content-Type" : "Text-Plain"});    
+    res.writeHead(200, {"Content-Type" : "text/plain"});    
     res.end("Pagina Listar eventos");
   }
   else if(req.url === '/deleteEvent')
   {
-    res.writeHead(200, {"Content-Type" : "Text-Plain"});    
+    res.writeHead(200, {"Content-Type" : "text/plain"});    
     res.end("Pagina Deletar Eventos");   
   }
   else if(req.url === "/evaluateNewEvent")
   {
-    res.writeHead(200, {"Content-Type": "Text-Plain"});
+    res.writeHead(200, {"Content-Type": "text/plain"});
     res.end("Pagina de Avaliar novos eventos");
   }
   else if(req.url === '/addFunds')
   {
-    res.writeHead(200, {"Content-Type" : "Text-Plain"});
+    res.writeHead(200, {"Content-Type" : "text/plain"});
     res.end("Pagina de Adicionar Fundos");
   }
   else if(req.url === '/withdrawFunds')
   {
-    res.writeHead(200, {'Content-Type' : "Text-Plain"});
+    res.writeHead(200, {'Content-Type' : "text/plain"});
     res.end("Pagina de Sacar Fundos");
   }
   else if(req.url === '/betOnEvent')
   {
-    res.writeHead(200, {"Content-Type" : "Text-Plain"});
+    res.writeHead(200, {"Content-Type" : "text/plain"});
     res.end("Pagina de Apostar em evento");
   }
   else if(req.url === '/finishEvent')
   {
-    res.writeHead(200, {"Content-Type" : "Text-Plain"});
+    res.writeHead(200, {"Content-Type" : "text/plain"});
     res.end("Pagina de Encerrar Evento");
   }
   else if(req.url === '/searchEvent')
   {
-    res.writeHead(200, {"Content-Type" : "Text-Plain"});
+    res.writeHead(200, {"Content-Type" : "text/plain"});
     res.end("Pagina de Busca de Eventos");
   }
   
