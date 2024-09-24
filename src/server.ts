f3a0de5bf0f8333b6bba2f49a7dcd0c55a6244aa
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-09-23 21:12:03 -0300

    Criando Rotas

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,62 @@
 import http from 'http';
 
 const server = http.createServer((req, res) =>{
-  res.writeHead(200, {"Content-Type" : "Text-Plain"});
-  res.end("Servidor da PucBet funcionando");
+  if(req.url === '/signUp')
+  {
+    res.writeHead(200, {"Content-Type" : "Text-Plain"});
+    res.end("Pagina de Sign UP");
+  }
+  else if(req.url === '/login')
+  {
+    res.writeHead(200, {"Content-Type" : "Text-Plain"});
+    res.end("Pagina de Login");
+  }
+  else if(req.url === '/addNewEvent')
+  {
+    res.writeHead(200, {"Content-Type" : "Text-Plain"});
+    res.end("Pagina de Novo Evento");
+  }
+  else if(req.url === '/getEvent')
+  {
+    res.writeHead(200, {"Content-Type" : "Text-Plain"});    
+    res.end("Pagina Listar eventos");
+  }
+  else if(req.url === '/deleteEvent')
+  {
+    res.writeHead(200, {"Content-Type" : "Text-Plain"});    
+    res.end("Pagina Deletar Eventos");   
+  }
+  else if(req.url === "/evaluateNewEvent")
+  {
+    res.writeHead(200, {"Content-Type": "Text-Plain"});
+    res.end("Pagina de Avaliar novos eventos");
+  }
+  else if(req.url === '/addFunds')
+  {
+    res.writeHead(200, {"Content-Type" : "Text-Plain"});
+    res.end("Pagina de Adicionar Fundos");
+  }
+  else if(req.url === '/withdrawFunds')
+  {
+    res.writeHead(200, {'Content-Type' : "Text-Plain"});
+    res.end("Pagina de Sacar Fundos");
+  }
+  else if(req.url === '/betOnEvent')
+  {
+    res.writeHead(200, {"Content-Type" : "Text-Plain"});
+    res.end("Pagina de Apostar em evento");
+  }
+  else if(req.url === '/finishEvent')
+  {
+    res.writeHead(200, {"Content-Type" : "Text-Plain"});
+    res.end("Pagina de Encerrar Evento");
+  }
+  else if(req.url === '/searchEvent')
+  {
+    res.writeHead(200, {"Content-Type" : "Text-Plain"});
+    res.end("Pagina de Busca de Eventos");
+  }
+  
 });
 
 server.listen(3000, () =>{
