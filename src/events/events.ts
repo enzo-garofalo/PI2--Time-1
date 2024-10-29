90198b6418ce86d6f0ca8f396128ac1dbd3e4cea
PedroDaou <pedro.mpd@puccampinas.edu.br>
2024-10-29 19:30:02 -0300

    getEvent Route

--- src/events/events.ts
+++ src/events/events.ts
@@ -1,4 +1,5 @@
 import {Request, RequestHandler, Response} from "express";
+import { dbEventsManager } from "../db/databaseEvent";
 import { DataBaseManager } from "../db/connection";
 import OracleDB from "oracledb";
 
@@ -10,82 +11,46 @@ export namespace EventsManager
         idEvent: number|undefined,
         title: string,
         description: string,
-        isValid: number,
-        categories: string
+        status_event: number,
+        categories: string,
+        register_date: string | undefined,
+        bets_funds: number,
+        finish_date: string
     }
-
-
 
     //função que coloca o evento no BD (medina)
-    async function addNewEvent(event:Event){
-
-        OracleDB.outFormat = OracleDB.OUT_FORMAT_OBJECT;
-
-        const connection:OracleDB.Connection = 
-            await DataBaseManager.get_connection();
-        
-        let attempts = 3;
-        let sucessfull = false;
-
-        while(attempts > 0){
-            try {
-                await connection.execute(
-                    `INSERT INTO EVENTS
-                    (ID_EVENT, TITLE, DESCRIPTION, CATEGORIES, ISVALID)
-                    VALUES
-                    (
-                    SEQ_EVENTS.NEXTVAL,
-                    :title, :description, :categories, :isValid
-                    )`,
-                    {
-                        title: event.title,
-                        description: event.description,
-                        categories: event.categories,
-                        isValid: event.isValid
-                    }
-                );
-                
-                
-            console.log('Novo Evento criado. Pendente aprovação.')
-            sucessfull = true;
-            break;
-            }catch(error){
-                console.error(error);
-                attempts--;
-            }
-        }
-        await connection.commit();
-        await connection.close();
-
-        return sucessfull;
-    }
 
     export const addNewEventHandler: RequestHandler = 
-        async (req: Request, res: Response) => {
+    async (req: Request, res: Response) => {
 
+        if(req.session.role === undefined){
+            console.log(req.session.token);
+            res.statusCode = 401;
+            res.send('Usuário não está logado!');
+            return;
+        }
+        
         const pTitle = req.get('Title');
         const pDescription = req.get('Description');
         const pCategories = req.get('Categories');
+        const pFinishDate = req.get('finishDate');
 
-        if(pTitle && pDescription && pCategories)
+        if(pTitle && pDescription && pCategories && pFinishDate)
         {
             const newEvent: Event = 
             {
                 idEvent: undefined,
                 title: pTitle,
                 description: pDescription,
-                isValid: 0,
-                categories: pCategories
+                status_event: 0,
+                categories: pCategories,
+                register_date: undefined,
+                bets_funds: 0.00,
+                finish_date: pFinishDate
             }
-
-            if (await addNewEvent(newEvent))
-            {
-                req.statusCode = 200;
-                res.send("Novo Evento adicionado.");
-            }else{
-                res.statusCode = 409;
-                res.send("Erro inesperado ao criar o evento");
-            }
+            await dbEventsManager.addNewEvent(newEvent);
+            req.statusCode = 200;
+            res.send("Novo Evento adicionado.");
         }else{
             res.statusCode = 400;
             res.send("Parâmetros inválidos ou faltantes");
@@ -158,6 +123,69 @@ export namespace EventsManager
             return;
         }
     }   
+
+    export const finishEventHandler: RequestHandler =
+    async (req: Request, res: Response) => {
+        // e distribuir os fundos dos apostadores proporcionalmente aos vencedores). 
+        if(!req.session.token)
+        {
+            res.statusCode = 401;
+            res.send("Usuário não está logado");
+        }else if(req.session.role === 0){
+            res.statusCode = 401;
+            res.send("Rota permitida apenas para moderadores");
+        }
+
+        const pIdEvent = Number(req.get('eventId'));
+        const pVerdictCode = Number(req.get('verdictCode'));
+        // 1 para aconteceu 2 para não aconteceu
+
+        if(pIdEvent && pVerdictCode){
+            await dbEventsManager.finishEvent(pIdEvent, pVerdictCode);
+            await dbEventsManager.shareEventFunds(pIdEvent, pVerdictCode);
+
+            res.statusCode = 200;
+            res.send('Evento finalizado e ganhos distribuídos!');
+        }else{
+            res.statusCode = 400;
+            res.send("Formato de requisição inválido.");
+        }        
+    };    
+
+    export const searchEventHandler: RequestHandler =
+    async (req: Request, res: Response) => {
+
+        if(!req.session.role){
+            res.statusCode = 401;
+            res.send('Usuário não está logado!');
+            return;
+        }
+        
+        const pStringBusca = req.get('title_request')
+
+        if(pStringBusca){
+            try{
+                
+                const events = await dbEventsManager.searchEvent(pStringBusca);
+
+                if(events){
+                    res.statusCode = 404;
+                    res.send('Evento não encontrado.');
+                }else{
+                    res.statusCode = 200;
+                    res.send(events);
+                }
+                
+            } catch (error){
+                console.error('Erro no servidor ao encontrar eventos.', error);
+            }
+        }else{
+            res.statusCode = 400;
+            res.send('Erro inesperado ao realizar busca de eventos');
+        }
+        
+            
+    }
 
     export const deleteEventHandler: RequestHandler = 
      async (req: Request, res: Response) => {
@@ -235,4 +263,61 @@ export namespace EventsManager
             await connection.close();
         }   
     }        
+
+    export const getEventHandler: RequestHandler = 
+    async (req: Request, res: Response) => {
+        
+        if (req.session.role === undefined) {
+            res.statusCode = 401;
+            res.send('Usuário não está logado!');
+            return;
+        }
+
+        const pStatus = req.query.status as string;
+        let consulta = '';
+
+        const connection: OracleDB.Connection = await DataBaseManager.get_connection();
+
+        try {
+            switch (pStatus) {
+                case "aguardando aprovação":
+                    consulta = `SELECT ID_EVENT, TITLE, DESCRIPTION, CATEGORIES,
+                                REGISTER_DATE, FINISH_DATE
+                                FROM EVENTS
+                                WHERE STATUS_EVENT = 0`;
+                    break;
+
+                case "eventos já ocorridos":
+                    consulta = `SELECT ID_EVENT, DESCRIPTION, CATEGORIES,
+                                REGISTER_DATE, FINISH_DATE
+                                FROM EVENTS
+                                WHERE STATUS_EVENT = 1 AND FINISH_DATE < SYSDATE`;
+                    break;
+
+                case "eventos futuros":
+                    consulta = `SELECT ID_EVENT, DESCRIPTION, CATEGORIES,
+                                REGISTER_DATE, FINISH_DATE
+                                FROM EVENTS
+                                WHERE STATUS_EVENT = 1 AND FINISH_DATE >= SYSDATE`;
+                    break;
+
+                default:
+                    res.statusCode = 400;
+                    res.send("Status inválido. Tente usar 'aguardando_aprovacao', 'ocorridos' ou 'futuros'.");
+                    return;
+            }
+
+            const result: OracleDB.Result<Event> = await connection.execute(consulta);
+            res.statusCode = 200;
+            res.send(result.rows);
+
+        } catch (error){
+            console.error("Erro ao obter eventos:", error);
+            res.statusCode = 500;
+            res.send("Erro ao processar a requisição");
+
+        } finally{
+            await connection.close();
+        }
+    }
 }
