5cc6dd57628bebf4b888e397e208fad77c044fd9
RogerioMedina <roger.m2@puccampinas.edu.br>
2024-10-30 21:20:36 -0300

    connections limpada

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -24,20 +24,6 @@ export namespace DataBaseManager
         return connection;
     }
 
-    export async function getUserID(token:string)
-    {
-        const connection = await get_connection()
-        
-        const userID : OracleDB.Result<number>  = 
-            await connection.execute(
-                `SELECT ID FROM ACCOUNTS
-                WHERE TOKEN = :token`,
-                {token}
-        );
-
-        return userID.rows;
-    }
-
     export async function getUserByToken(token: string)
     {
         const connection = await get_connection()
@@ -54,8 +40,7 @@ export namespace DataBaseManager
         await connection.close();
         return account.rows;
     }
-    
-    
+        
     export async function getIdWallet(id_user:number) 
     {
         
@@ -68,110 +53,6 @@ export namespace DataBaseManager
         );
         return idWallet.rows;
 
-    }
-
-    export async function getSaldoAtual(idWallet: number) 
-    {
-        //verificar saldo atual
-        OracleDB.outFormat = OracleDB.OUT_FORMAT_OBJECT;
-        const connection:OracleDB.Connection = 
-        await DataBaseManager.get_connection();
-
-        const result: OracleDB.Result<{BALANCE: number}> = 
-        await connection.execute(
-            'SELECT BALANCE FROM WALLET WHERE ID = :idWallet',
-            {idWallet}
-        ); 
-
-        await connection.close();
-        return result.rows;
-    }
-
-    export async function newfoundWithdraw(qtdSacar: number, idWallet:number) 
-    {
-        
-        const connection:OracleDB.Connection = 
-        await DataBaseManager.get_connection();
-        
-
-        await connection.execute(
-        `
-            INSERT INTO HISTORIC(
-            TRANSACTION_ID,
-            TRANSACTION_TYPE,
-            TRANSACTION_VALUE,
-            FK_ID_WALLET
-            )values(
-            SEQ_TRANSACTION.NEXTVAL,
-            'SACAR',
-            :-qtdSacar,
-            :idWallet
-            )`,
-            {qtdSacar, idWallet}
-        );
-
-        await connection.commit();
-        await connection.close();
-    }
-
-    export async function getBalanceById(id_wallet : number)
-    {
-        const connection:OracleDB.Connection = await get_connection();
-
-        const balance : OracleDB.Result<{BALANCE: number}>  = 
-            await connection.execute(
-                `
-                SELECT SUM(TRANSACTION_VALUE) AS BALANCE
-                FROM HISTORIC 
-                WHERE FK_ID_WALLET = :id_wallet
-                `,
-                {id_wallet}
-        );
-
-        await connection.close;
-        return balance.rows;
-    }
-
-    export async function refreshBalance(id_wallet:number) 
-    {
-        const connection:OracleDB.Connection = await get_connection();
-        const balance = await getBalanceById(id_wallet);
-        
-        if(balance){
-            await connection.execute(
-                `
-                UPDATE WALLET
-                SET BALANCE = :balance
-                WHERE ID_WALLET = :id_wallet
-                `,
-                {
-                    balance: balance[0].BALANCE, 
-                    id_wallet: id_wallet
-                }
-            );
-        }
-        await connection.commit();
-        await connection.close();
-    }
-
-    export async function getNewEvents()
-    {
-        OracleDB.outFormat = OracleDB.OUT_FORMAT_OBJECT;
-
-        const connection:OracleDB.Connection = 
-            await DataBaseManager.get_connection();
-
-        const newEventsList: OracleDB.Result<Event> = 
-            await connection.execute(
-                `
-                SELECT ID_EVENT, TITLE, DESCRIPTION, CATEGORIES 
-                FROM EVENTS
-                WHERE status_event = 0
-                `
-        );
-        await connection.close();
-        console.log("Eventos retornados:", newEventsList.rows);
-        return newEventsList.rows;
     }
 
     export async function joinTables(token:string) {
@@ -188,7 +69,5 @@ export namespace DataBaseManager
         );
 
         return userID.rows;
-        
     }
-    
 }
