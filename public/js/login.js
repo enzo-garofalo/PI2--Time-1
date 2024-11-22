7355fff6f74f22c755aaae1e057cd6bda6503f7e
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-11-22 19:59:28 -0300

    Solving Merge bugs

--- public/js/login.js
+++ public/js/login.js
@@ -1,20 +1,5 @@
-function showMessage(content, type) {
-    const alertBox = document.getElementById('alert-box');
-    const alertMessage = document.getElementById('alert-message');
-    
-    alertMessage.innerHTML = content;
-    
-    // Exibe o alerta
-    alertBox.classList.remove('d-none');
-    alertBox.classList.add('show');
-}
+import { showAlert } from './signup.js'
 
-// Função para esconder a mensagem
-function hideMessage() {
-    const mb = document.getElementById('message-box');
-    mb.classList.remove('show');
-    mb.classList.add('hide');
-}
 
 // Validação para login
 function isValid(email, password) {
@@ -31,7 +16,11 @@ function isValid(email, password) {
     return valid;
 }
 
-
+// Função para validar o formato do email usando regex
+function isValidEmail(email) {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return emailRegex.test(email);
+}
 
 // Função de login
 async function signIn() {
@@ -58,37 +47,8 @@ async function signIn() {
             window.location.href = '../view/home.html';
         } else {
             const errorMessage = await res.text(); // Lê o erro retornado do servidor
-            showMessage(errorMessage, 'error');
-        }
-    }
-}
-// Função de cadastro
-async function signUp() {
-    var name = document.getElementById('fName').value;
-    var email = document.getElementById('fEmail').value;
-    var password = document.getElementById('fPassword').value;
-    var birthdate = document.getElementById('fBirthdate').value;
-
-    if (isValidSignUp(name, email, password, birthdate)) {
-        const signUpData = new Headers();
-        signUpData.append('name', name);
-        signUpData.append('email', email);
-        signUpData.append('password', password);
-        signUpData.append('birthdate', birthdate);
-
-        const res = await fetch(
-            'http://127.0.0.1:3000/signUp', {
-                method: 'PUT',
-                headers: signUpData,
-                credentials: 'include' // Necessário para enviar cookies entre cliente e servidor
-            }
-        );
-
-        if (res.ok) {
-            window.location.href = '../view/home.html';
-        } else {
-            const errorMessage = await res.text(); // Lê o erro retornado do servidor
-            showMessage(errorMessage, 'error');
+            // showMessage(errorMessage, 'error');
+            showAlert(errorMessage,'warning');
         }
     }
 }
